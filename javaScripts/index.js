d1f47038c17e1fb88b15dafb50098f2cb954a00e
Guy <guymoustapha@gmail.com>
2021-03-12 10:58:21 +0200

    get element using dom

--- javaScripts/index.js
+++ javaScripts/index.js
@@ -3,3 +3,11 @@ $(document).ready(function() {
     $('.toggle').toggleClass('active')
   })
 })
+
+document.querySelector('.contact-form').addEventListener('submit', submitForm);
+function submitForm(e) {
+  e.preventDefault();
+  let name = document.querySelector('.f-name').value;
+  let email = document.querySelector('.f-email').value;
+  let message= document.getElementsByName('message').value;
+}
